2d636dabea27e5b0a59176fc9747f053ae404f72
dumprahul <shanrahul04@gmail.com>
2025-06-07 07:10:34 +0530

    civic-integration-test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import { createCivicAuthPlugin } from "@civic/auth/nextjs"
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withCivicAuth = createCivicAuthPlugin({
+  clientId: "86634583-cf04-4ea9-836c-a1128eeac46e"
+});
+
+export default withCivicAuth(nextConfig)
